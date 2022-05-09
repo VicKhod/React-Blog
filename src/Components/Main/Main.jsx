aec9c51a3c3837c40cec34d0e6bfe2e2191d2ae9
VicKhod <vickhodulova@gmail.com>
2022-05-10 03:21:58 +0900

    Link to one post

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,13 +1,13 @@
 import React, {useState, useEffect} from "react";
 import api from "../../Api";
 import Card from "../Card/Card";
-import "./Main.css"
+import {Link} from "react-router-dom";
+import "./Main.css";
 
 const Main = ({search}) => {
     const [posts, getPosts] = useState([]);
     const [cards, updateCards] = useState(posts);
     
-
     useEffect(() => {          
         api.getPostList().then(ans =>{
             getPosts(ans);
@@ -20,8 +20,10 @@ const Main = ({search}) => {
         <>
             {search && <div className='search__item'>По запросу <strong>{search}</strong> найдено {cards.length} постов</div>}
             <div className="card__container">
-                {cards.map((post) =>
-                    <Card key={post._id} data={{...post}}/>
+                {cards.map((post) => 
+                    <Link to={"/post/ + post._id"} key={post._id}>
+                        <Card key={post._id} data={{...post}}/>
+                    </Link>  
                 )}
             </div>
         </>
